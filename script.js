d7fe80d754c577ad2fa8f05fc05cf28c75deec38
henixmehta <henumehta03@example.com>
2024-04-28 20:53:04 +0530

    SWEET LAERT

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
+// document.addEventListener("click",function click(){
+//     let c = confirm("Are you sure??");
+//     console.log(c);
+// })
 
-    // document.addEventListener("click",function click(){
-    //     let c = confirm("Are you sure??");
-    //     console.log(c);
-    // })
-    
-    //jquery        
+//jquery
+//YES
+$(document).ready(function () {
+  function askConfirmation() {
+    Swal.fire({
+      title: "YOU LOVE ME",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swal.fire(
+            'YOU : I LOVE YOU TOO',
+            'You clicked the button!',
+            'success'
+          )          
+      } else {
+        askConfirmation();
+      }
+    });
+  }
+  // NO
+  function ansNo() {
+    Swal.fire({
+      title: "ITS NOT RIGHT ANSWER",
+      text: "MY HEART SAYS YOU LOVE ME.",
+      icon: "warning",
+      confirmButtonText: "Got it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        ansNo();
+      }
+    });
+  }
 
-    $(document).ready(function () {
-        $('#btn1').click(function () {
-            let c = confirm("are you sure??");
-            console.log(c);
-        });
-        $('#btn2').click(function () {
-            alert("its ok its hurts me leave me alone");
-        });
-    })
+  $("#btn1").click(function () {
+    askConfirmation();
+  });
+
+  $("#btn2").click(function () {
+    ansNo();
+  });
+});
